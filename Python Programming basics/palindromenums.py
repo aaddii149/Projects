4cbefe23851b9dc48b99e35d3137d78279a14d22
Aditya Hariharan <aditya12320@gmail.com>
2025-02-02 18:32:17 +0530

    Commit for palindromenumber.py

---
+++ Python Programming basics/palindromenums.py
@@ -0,0 +1,19 @@
+def palindrome(num:int):
+    rev = 0
+    num1 = num
+    while (num1 > 0):
+        rev = rev*10 + num1%10
+        num1 = num1//10
+
+    if (rev== num):
+        return(True)
+    else:
+        return(False)
+    
+num = input("Please input a positive integer:")
+check = palindrome(int(num))
+if (check):
+    print(num + " is a Palindrome")
+else:
+    print(num + " is not a Palindrome")
+
