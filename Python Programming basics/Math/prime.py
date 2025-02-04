1b2ebe5722191c8dddb05f1c0585192e8e0e6d2c
Aditya Hariharan <aditya12320@gmail.com>
2025-02-04 09:16:51 +0530

    commit for modification in optimized method

--- Python Programming basics/Math/prime.py
+++ Python Programming basics/Math/prime.py
@@ -16,12 +16,9 @@ print(check1(2))
 def check2(a:int) -> bool:
     if (a == 1):
         return False
-    elif (a % 2 == 0 or a % 3 == 0):
-        return False
-    else:
-        for i in range(5, int(math.sqrt(a)), 6):
-            if(a % i == 0 or a % (i + 2) == 0):
-                return False
+    for i in range(2, 1 + int(math.sqrt(a))):
+        if (a % i == 0):
+            return False
     return True
 
-print(check2(1031))
+print(check2(3))
